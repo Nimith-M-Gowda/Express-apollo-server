eb0fdbc87f82f63a8f7e614323746f71ddab393c
Nimith-M-Gowda <nimithbbz10@gmail.com>
2021-11-06 17:35:44 +0530

    feat(mutations): aded resolvers for mutation

--- server/index.js
+++ server/index.js
@@ -2,14 +2,36 @@ const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
 const cors = require("cors");
 
-let users = {
+const uuid = require("uuid");
+
+let USERS = {
   1: {
     id: "1",
     username: "Nimith",
+    messageId: [1],
   },
   2: {
     id: "2",
     username: "Dave Davids",
+    messageId: [2, 3],
+  },
+};
+
+let MESSAGES = {
+  1: {
+    id: "1",
+    text: "message1",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "message2",
+    userId: "2",
+  },
+  3: {
+    id: "3",
+    text: "message2",
+    userId: "2",
   },
 };
 
@@ -23,11 +45,24 @@ let users = {
       me: User!
       user(id: ID!): User
       users: [User!]
+      messages: [Message!]
+      message(id: ID!): Message!
     }
     type User {
-      username: String!
-      age: Int
       id: ID!
+      username: String!
+      messages: [Message]
+    }
+    type Message {
+      id: ID!
+      text: String!
+      user: User!
+    }
+
+    type Mutation {
+      createMessage(text: String!): Message!
+      deleteMessage(id: ID!): Boolean!
+      updateMessage(id: ID!, text: String!): Message!
     }
   `;
 
@@ -37,29 +72,92 @@ let users = {
         return me;
       },
       user: (parent, { id }) => {
-        return users[id];
+        return USERS[id];
       },
       users: () => {
-        return Object.values(users);
+        return Object.values(USERS);
+      },
+      messages: () => {
+        return Object.values(MESSAGES);
+      },
+      message: (parent, { id }) => {
+        return MESSAGES[id];
       },
     },
+    Message: {
+      user: (parent, { id }) => {
+        return USERS[parent.userId];
+      },
+    },
+    User: {
+      messages: (parent, { id }) => {
+        let resultArray = [];
+        parent.messageId.map((e) => {
+          resultArray.push(MESSAGES[e]);
+        });
+        return resultArray;
+        // return Object.values(messages).filter(
+        //   (eachmessage) => eachmessage.userId === parent.id
+        // );
+      },
+    },
+
     // User: {
     //   username: (parent) => {
     //     parent.username = `${parent.username}+rock`;
     //     return parent.username;
     //   },
     // },
+    Mutation: {
+      createMessage: (parent, { text }, { me }) => {
+        const id = uuid.v4();
+        const message = {
+          id,
+          text,
+          userId: me.id,
+        };
+        MESSAGES[id] = message;
+        USERS[me.id].messageId.push(id);
+        return message;
+      },
+
+      // deleteMessage: (parent, { id }, { me }) => {
+      //   if (Object.keys(MESSAGES).includes(id)) {
+      //     delete MESSAGES[`${id}`];
+      //     // USERS[me.id].messageId.filter((e) => e != id);
+
+      //     return true;
+      //   }
+      //   return false;
+      // },
+
+      deleteMessage: (parent, { id }, { me }) => {
+        const { [id]: message, ...otherMessages } = MESSAGES;
+        if (!message) {
+          return false;
+        }
+        MESSAGES = otherMessages;
+        return true;
+      },
+
+      updateMessage: (parent, { id, text }, { me }) => {
+        const message = {
+          id,
+          text,
+          userId: me.id,
+        };
+
+        MESSAGES[id] = message;
+        return message;
+      },
+    },
   };
 
   const server = new ApolloServer({
     typeDefs: schema,
     resolvers,
     context: {
-      me: {
-        id: 1,
-        username: "Nimith M Gowda",
-        age: 24,
-      },
+      me: USERS[1],
     },
   });
 
